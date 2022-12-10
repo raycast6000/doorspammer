cfcef2fd6332e7789ec665f21f64a2576c7dde1d
Raycast <raycast6000@gmail.com>
2022-12-10 00:14:37 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,7 @@
+assert(rconsolewarn, "Your exploit does not support console functions.")
+
 xpcall(function()
    for _ = 0, 2 do -- For some reason you need to run twice the script to get it working.
       loadstring(game:HttpGet("https://raw.githubusercontent.com/raycast6000/doorspammer/main/src.lua"))() 
    end
-end)
+end, function(e) rconsolewarn(e) end)
