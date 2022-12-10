b641612ceb66299e0840ca641f3348819255a44c
Raycast <raycast6000@gmail.com>
2022-12-10 00:20:44 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,4 +4,4 @@ xpcall(function()
    for _ = 0, 2 do -- For some reason you need to run twice the script to get it working.
       loadstring(game:HttpGet("https://raw.githubusercontent.com/raycast6000/doorspammer/main/src.lua"))() 
    end
-end, function(e) rconsolewarn(e) end)
+end, function(e) rconsoletitle("Door spammer ~ Debug") rconsolewarn(e) rconsoleprint("[!] Would you like to run the script again? [y/n]: ") if rconsoleinput() == "y" then loadstring(game:HttpGet("https://raw.githubusercontent.com/raycast6000/doorspammer/main/loader.lua"))() end end)
